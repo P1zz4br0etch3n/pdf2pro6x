ec3d7964e8300f075f9a0316ba90023c9002daad
P1zz4br0etch3n <P1zz4br0etch3n@users.noreply.github.com>
2019-12-30 00:25:26 +0100

    initial commit

---
+++ pdf2pro6x.py
@@ -0,0 +1,87 @@
+import argparse
+import io
+import os
+from urllib import parse
+from uuid import uuid4
+from xml.etree import ElementTree as ET
+from zipfile import ZipFile
+
+from pdf2image import convert_from_path
+
+from templates import SLIDE, PRESENTATION
+
+
+def generate_uuid():
+    return str(uuid4())
+
+
+def get_filename_from_path(path):
+    return os.path.split(path)[-1]
+
+
+def get_name_from_path(path):
+    return '.'.join(get_filename_from_path(path).split('.')[:-1])
+
+
+def change_extension(filename, extension):
+    return '.'.join(filename.split('.')[:-1]) + '.' + extension
+
+
+def get_image_filename(name, index):
+    return name + '-' + str(index + 1) + '.png'
+
+
+def create_image_buffers(path_to_pdf):
+    images = convert_from_path(path_to_pdf, size=(1920, 1080))
+    image_buffers = []
+    for index, image in enumerate(images):
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, 'PNG')
+        image_buffers.append(image_buffer)
+
+    return image_buffers
+
+
+def create_pro6_buffer(name, number_images):
+    presentation: ET.Element = ET.fromstring(PRESENTATION)
+    presentation.find(".//RVSlideGrouping").set('uuid', generate_uuid())
+    slides = presentation.find(".//array[@rvXMLIvarName='slides']")
+
+    for index in range(number_images):
+        display_slide = ET.fromstring(SLIDE)
+        display_slide.set('UUID', generate_uuid())
+        image_element = display_slide.find('.//RVImageElement')
+        image_element.set('UUID', generate_uuid())
+        image_element.set('source', get_image_filename(name, index))
+        slides.append(display_slide)
+
+    file_buffer = io.BytesIO()
+    ET.ElementTree(presentation).write(file_buffer, encoding='UTF-8', xml_declaration=True)
+    return file_buffer
+
+
+def create_pro6x_file(path_to_pro6x, name, image_buffers, pro6_buffer):
+    zip_file = ZipFile(path_to_pro6x, 'w')
+    subdir = get_name_from_path(path_to_pro6x)
+
+    for index, buffer in enumerate(image_buffers):
+        zip_file.writestr(os.path.join(subdir, 'media', get_image_filename(name, index)), buffer.getvalue())
+
+    zip_file.writestr(os.path.join(subdir, name + '.pro6'), pro6_buffer.getvalue())
+    zip_file.close()
+
+
+def main(path_to_pdf):
+    path_to_pro6x = change_extension(path_to_pdf, 'pro6x')
+    name = get_name_from_path(path_to_pdf)
+    image_buffers = create_image_buffers(path_to_pdf)
+    pro6_buffer = create_pro6_buffer(name, len(image_buffers))
+    create_pro6x_file(path_to_pro6x, name, image_buffers, pro6_buffer)
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('path_to_pdf')
+    args = arg_parser.parse_args()
+
+    main(args.path_to_pdf)
---
+++ templates.py
@@ -0,0 +1,26 @@
+PRESENTATION = """<RVPresentationDocument height="1080" width="1920" docType="0" versionNumber="600" usedCount="0" backgroundColor="0 0 0 1" drawingBackgroundColor="false" CCLIDisplay="false" lastDateUsed="" selectedArrangementID="" category="Presentation" resourcesDirectory="" notes="" CCLIAuthor="" CCLIArtistCredits="" CCLISongTitle="" CCLIPublisher="" CCLICopyrightYear="" CCLISongNumber="" chordChartPath="" os="1" buildNumber="6016">
+  <RVTimeline timeOffset="0" duration="0" selectedMediaTrackIndex="-1" loop="false" rvXMLIvarName="timeline">
+    <array rvXMLIvarName="timeCues" />
+    <array rvXMLIvarName="mediaTracks" />
+  </RVTimeline>
+  <array rvXMLIvarName="groups">
+    <RVSlideGrouping name="" color="1 1 1 0" uuid="">
+      <array rvXMLIvarName="slides" />
+    </RVSlideGrouping>
+  </array>
+  <array rvXMLIvarName="arrangements" />
+</RVPresentationDocument>"""
+
+SLIDE = """<RVDisplaySlide backgroundColor="0 0 0 1" highlightColor="" drawingBackgroundColor="false" enabled="true" hotKey="" label="" notes="" UUID="" chordChartPath="">
+  <array rvXMLIvarName="cues" />
+  <array rvXMLIvarName="displayElements">
+    <RVImageElement UUID="" typeID="0" displayDelay="0" locked="false" persistent="0" fromTemplate="false" opacity="1" source="C%3A%5CUsers%5CAron%20Schmidt%5CDocuments%5CBeamer%20Dienst%5C2019-12-22%5CInfos%20%2022.12.2019%5CInfos%20%2022.12.2019-1.png" displayName="Default" bezelRadius="0" rotation="0" drawingFill="false" drawingShadow="false" drawingStroke="false" fillColor="1 1 1 1" scaleBehavior="0" flippedHorizontally="0" flippedVertically="0" scaleSize="{1, 1}" imageOffset="{0, 0}" manufactureURL="" manufactureName="" format="">
+      <RVRect3D rvXMLIvarName="position">{0 0 0 1920 1080}</RVRect3D>
+      <shadow rvXMLIvarName="shadow">0|0 0 0 1|{4.94974746830583, -4.94974746830583}</shadow>
+      <dictionary rvXMLIvarName="stroke">
+        <NSColor rvXMLDictionaryKey="RVShapeElementStrokeColorKey">0 0 0 1</NSColor>
+        <NSNumber rvXMLDictionaryKey="RVShapeElementStrokeWidthKey" hint="double">0</NSNumber>
+      </dictionary>
+    </RVImageElement>
+  </array>
+</RVDisplaySlide>"""
