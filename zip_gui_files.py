c2a764d331a3d5e6dc2aedbc59d165618e4f0181
P1zz4br0etch3n <P1zz4br0etch3n@users.noreply.github.com>
2020-04-10 22:50:28 +0200

    fix zip file creation

--- zip_gui_files.py
+++ zip_gui_files.py
@@ -1,13 +1,17 @@
 import argparse
+import os
 from zipfile import ZipFile
+
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('zip_file')
-    arg_parser.add_argument('contents', nargs="*")
+    arg_parser.add_argument('folder')
     args = arg_parser.parse_args()
 
     zip_file = ZipFile(args.zip_file, 'w')
-    for content in args.contents:
-        zip_file.write(content, content.replace('dist/pdf2pro6x_gui/', ''))
+    for root, dirs, files in os.walk(args.folder):
+        for file in files:
+            file = os.path.join(root, file)
+            zip_file.write(file, file.replace(args.folder, ''))
     zip_file.close()
